e0a39f6cc8a8c66ef3224318a3403a8cc2c64ee0
ahmed <ahmedismailm85@gmail.com>
2022-03-25 07:09:51 +0200

    eb init

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,2 +1,3 @@
 # aws s3 cp deploy.zip s3://udagram-pro/deploy.zip && aws elasticbeanstalk create-application-version --application-name udagram --source-bundle S3Bucket="udagram-pro",S3Key="deploy.zip" && aws elasticbeanstalk update-environment --application-name udagram --environment-name aim-env
+eb init --region us-east-1 --platform Node.js udagram-api
 eb deploy
