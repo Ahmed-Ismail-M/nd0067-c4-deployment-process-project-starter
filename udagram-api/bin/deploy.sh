c20fe58ea8deffc4c8d922b1c3259b8c990f0a78
ahmed <ahmedismailm85@gmail.com>
2022-03-26 19:00:57 +0200

    deploy without bin

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,3 +1,2 @@
-# aws s3 cp deploy.zip s3://udagram-pro/deploy.zip && aws elasticbeanstalk create-application-version --application-name udagram --source-bundle S3Bucket="udagram-pro",S3Key="deploy.zip" && aws elasticbeanstalk update-environment --application-name udagram --environment-name aim-env
-eb init --region us-east-1 --platform Node.js udagram-api
+# eb init --region us-east-1 --platform Node.js udagram-api --source www/Archive.zip
 eb deploy 
